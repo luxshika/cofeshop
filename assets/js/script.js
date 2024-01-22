d4eed566520ae9a833f1383fb4ec07ca1438d1c7
Luxshika Prashath <89829659+luxshika@users.noreply.github.com>
2024-01-22 15:53:22 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const navbarContent =  document.getElementById("navbarContent");
+const navbarMenuIcon =  document.getElementById("navbarMenuIcon");
+
+navbarMenuIcon.onclick = function(){
+  navbarContent.classList.toggle("active")
+}
